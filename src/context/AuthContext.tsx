d12968a8a96a8ccb0199587cd85d022d9f7aa045
Khang Tran <creamybanana@Khang.local>
2024-08-10 16:31:00 -0700

    added authentification for user

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,3 +1,6 @@
+import { getCurrentUser } from "@/lib/appwrite/api";
+import { IUser } from "@/lib/types";
+import { Vault } from "lucide-react";
 import { createContext, useContext, useEffect, useState } from "react";
 
 export const INITIAL_USER = {
@@ -15,10 +18,51 @@ const INTIAL_STATE = {
   isAuthenticated: false,
   setUser: () => {},
   setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean,
 };
 
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+const AuthContext = createContext<IContextType>(INTIAL_STATE);
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+ const checkAuthUser: async () => {
+    try {
+      const currentAccount = await getCurrentUser();
+
+      if (currentAccount) {
+        setUser({
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          username: currentAccount.username,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio,
+        });
+        setIsAuthenticated(true);
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated,
+    checkAuthUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export default AuthContext;
